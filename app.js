202bde7fe41e69ffa2ec4309ab7958b4efa96d2b
3mrePala <palaemre27@gmail.com>
2022-08-28 19:45:27 +1000

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+function convertCelsiusToFahrenheit (celsius) {
+    let fahrenheit = celsius * 1.8 + 32
+    return fahrenheit
+}
+
+console.log (convertCelsiusToFahrenheit(0))
+console.log (convertCelsiusToFahrenheit(10))
+console.log (convertCelsiusToFahrenheit(30))
+ 
